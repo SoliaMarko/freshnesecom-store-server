5b5baa0e401a79adb9bc0a1890a8ee430780bc8f
solomiia-marko <soliamark25@gmail.com>
2024-04-16 15:34:45 +0300

    chore: set up configuration and extracted PORT to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {AuthModule} from './features/auth/auth.module';
-import { ProductModule } from './features/product/product.module';
-import { UserModule } from './features/user/user.module';
+import {ProductModule} from './features/product/product.module';
+import {UserModule} from './features/user/user.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService]
 })
