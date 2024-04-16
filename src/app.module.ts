da893c8fbc6cf300a78aa96fc0135316dcf64221
solomiia-marko <soliamark25@gmail.com>
2024-04-16 19:50:21 +0300

    feat: set up basic mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
+import {MongooseModule} from '@nestjs/mongoose';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {AuthModule} from './features/auth/auth.module';
 import {ProductModule} from './features/product/product.module';
-import {UserModule} from './features/user/user.module';
+import {UserModule} from 'features/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ProductModule, UserModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI), AuthModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ src/features/users/schemas/User.schema.ts
@@ -0,0 +1,24 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {HydratedDocument} from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({required: true})
+  firstName: string;
+
+  @Prop({required: true})
+  lastName: string;
+
+  @Prop({unique: true, required: true})
+  email: string;
+
+  @Prop({required: true})
+  password: string;
+
+  @Prop({required: false})
+  phoneNumber?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
