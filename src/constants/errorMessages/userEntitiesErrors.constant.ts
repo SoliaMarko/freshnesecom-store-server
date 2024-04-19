13b7be605bafc870e9b28c224a52298e71d9069a
solomiia-marko <soliamark25@gmail.com>
2024-04-19 13:53:28 +0300

    chore: fixed according to comments

--- src/constants/errorMessages/userEntitiesErrors.constant.ts
+++ src/constants/errorMessages/userEntitiesErrors.constant.ts
@@ -1,19 +1,27 @@
 import {passwordValidationRules} from '@constants/validationRules/validationRules.constant';
 
+const generateNotEnoughMessage = (min: number, input: string): string => {
+  return `For security reasons password must contain at least ${min} ${input}`;
+};
+
+const generateNotFoundMessage = (value: string): string => {
+  return `Could not find the user with such ${value}`;
+};
+
 export const errorMessages = {
   email: {
     ALREADY_REGISTERED: 'This email is already taken'
   },
   password: {
     NOT_ENOUGH: {
-      LOWERCASES: `For security reasons password must contain at least ${passwordValidationRules.LOWERCASE_MIN_COUNT} lowercase letters`,
-      UPPERCASES: `For security reasons password must contain at least ${passwordValidationRules.UPPERCASE_MIN_COUNT} uppercase letters`,
-      NUMBERS: `For security reasons password must contain at least ${passwordValidationRules.NUMBERS_MIN_COUNT} numbers`,
-      SYMBOLS: `For security reasons password must contain at least ${passwordValidationRules.LOWERCASE_MIN_COUNT} symbols`
+      LOWERCASES: generateNotEnoughMessage(passwordValidationRules.LOWERCASE_MIN_COUNT, 'lowercase letters'),
+      UPPERCASES: generateNotEnoughMessage(passwordValidationRules.UPPERCASE_MIN_COUNT, 'uppercase letters'),
+      NUMBERS: generateNotEnoughMessage(passwordValidationRules.NUMBERS_MIN_COUNT, 'numbers'),
+      SYMBOLS: generateNotEnoughMessage(passwordValidationRules.LOWERCASE_MIN_COUNT, 'symbols')
     }
   },
-  NOT_FOUND_BY_EMAIL: 'Could not find the user with such email',
-  NOT_FOUND_BY_ID: 'Could not find the user with such id'
+  NOT_FOUND_BY_EMAIL: generateNotFoundMessage('email'),
+  NOT_FOUND_BY_ID: generateNotFoundMessage('id')
 };
 
 export const emailError = errorMessages.email;
--- src/constants/swaggerConstants/userEntitiesDescriptions.constant.ts
+++ src/constants/swaggerConstants/userEntitiesDescriptions.constant.ts
@@ -1,8 +1,12 @@
 import {firstNameValidationRules, lastNameValidationRules, passwordValidationRules} from '@constants/validationRules/validationRules.constant';
 
+const generateMustBeInRangeMessage = (field = '', min: number, max: number): string => {
+  return `User's ${field}. Must be between ${min} and ${max} characters long`;
+};
+
 export const descriptions = {
-  FIRST_NAME_DESCRIPTION: `User's first name. Must be between ${firstNameValidationRules.MIN_LENGTH} and ${firstNameValidationRules.MAX_LENGTH} characters long`,
-  LAST_NAME_DESCRIPTION: `User's last name. Must be between ${lastNameValidationRules.MIN_LENGTH} and ${lastNameValidationRules.MAX_LENGTH} characters long`,
+  FIRST_NAME_DESCRIPTION: generateMustBeInRangeMessage('first name', firstNameValidationRules.MIN_LENGTH, firstNameValidationRules.MAX_LENGTH),
+  LAST_NAME_DESCRIPTION: generateMustBeInRangeMessage('last name', lastNameValidationRules.MIN_LENGTH, lastNameValidationRules.MAX_LENGTH),
   EMAIL_DESCRIPTION: 'Email, must be unique',
   PASSWORD_DESCRIPTION: `Password, must contain at least ${passwordValidationRules.MIN_LENGTH} characters: 
   ${passwordValidationRules.LOWERCASE_MIN_COUNT} lowercase letters, 
